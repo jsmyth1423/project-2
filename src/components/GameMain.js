07d6eb4506d7588c05e2c9fbafeb31aa0e81438d
Calum MacEachen <calum.maceachen@gmail.com>
2022-03-11 10:57:04 +0000

    All done except the game restart

--- src/components/GameMain.js
+++ src/components/GameMain.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Hints from './Hints';
 import { getAllAgents } from '../lib/api';
 
-
-
 const GameMain = () => {
   const [agents, setAgents] = React.useState([]);
   const [activeAgent, setActive] = React.useState(null);
@@ -12,7 +10,9 @@ const GameMain = () => {
     const getData = async () => {
       try {
         const { data } = await getAllAgents();
-        const playableAgents = data.data.filter((agent) => agent.isPlayableCharacter);
+        const playableAgents = data.data.filter(
+          (agent) => agent.isPlayableCharacter
+        );
         activate(playableAgents);
       } catch (err) {
         console.error(err);
@@ -35,14 +35,17 @@ const GameMain = () => {
   function compareChoice(e) {
     let isCorrect = false;
     for (let i = 0; i < activeAgent.abilities.length; i++) {
-      if (activeAgent.abilities[i].displayName.toLowerCase().replace(' ', '') === e.target.id) {
+      if (
+        activeAgent.abilities[i].displayName.toLowerCase().replace(' ', '') ===
+        e.target.id
+      ) {
         isCorrect = true;
       }
     }
     if (isCorrect === true) {
       if (window.confirm('Congrats, you won! Play again?')) {
         window.location.reload();
-      };
+      }
       console.log(`clicked: ${e.target.id}`);
     } else if (isCorrect === false) {
       window.alert('Bad luck, try again!');
@@ -57,11 +60,10 @@ const GameMain = () => {
     // console.log('inactive abilities', inactiveAbilities);
     const wrongAnswers = [];
 
-
     while (wrongAnswers.length < 3) {
       const newAnswer =
         inactiveAbilities[Math.floor(Math.random() * inactiveAbilities.length)][
-        Math.floor(Math.random() * 4)
+          Math.floor(Math.random() * 4)
         ];
       if (!wrongAnswers.includes(newAnswer)) {
         wrongAnswers.push(newAnswer);
@@ -75,7 +77,8 @@ const GameMain = () => {
   }
 
   function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+    let currentIndex = array.length,
+      randomIndex;
     // While there remain elements to shuffle...
     while (currentIndex !== 0) {
       // Pick a remaining element...
@@ -83,11 +86,12 @@ const GameMain = () => {
       currentIndex--;
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+        array[randomIndex],
+        array[currentIndex]
+      ];
     }
     return array;
   }
-
 
   return (
     <>
@@ -101,21 +105,50 @@ const GameMain = () => {
         )}
         <div className="container">
           <div className="hints-container">
-            <Hints activeAgent={activeAgent} /></div>
+            <Hints activeAgent={activeAgent} />
+          </div>
           <div className="agent-container">
-
             {!agents ? (
               <p>Loading...</p>
             ) : (
               <div className="has-text-centered">
-                {activeAgent && <img src={activeAgent.bustPortrait} className="agent-img" alt="agentpic"></img>}
+                {activeAgent && (
+                  <img
+                    src={activeAgent.bustPortrait}
+                    className="agent-img"
+                    alt="agentpic"
+                  ></img>
+                )}
               </div>
             )}
           </div>
-
         </div>
-        <div className='abilities-container'>
-          {!answers ? (<p>Loading Abilities...</p>) : (answers.map((answer) => (<div key={answer.displayName} className="answer-wrapper" onClick={compareChoice}> <div className="answer-text" id={answer.displayName.toLowerCase().replace(' ', '')}>{answer.displayName} </div><img alt='displayicon' src={answer.displayIcon} id={answer.displayName.toLowerCase().replace(' ', '')} className="ability-img" /></div>)))}
+        <div className="abilities-container">
+          {!answers ? (
+            <p>Loading Abilities...</p>
+          ) : (
+            answers.map((answer) => (
+              <div
+                key={answer.displayName}
+                className="answer-wrapper"
+                onClick={compareChoice}
+              >
+                {' '}
+                <div
+                  className="answer-text"
+                  id={answer.displayName.toLowerCase().replace(' ', '')}
+                >
+                  {answer.displayName}{' '}
+                </div>
+                <img
+                  alt="displayicon"
+                  src={answer.displayIcon}
+                  id={answer.displayName.toLowerCase().replace(' ', '')}
+                  className="ability-img"
+                />
+              </div>
+            ))
+          )}
         </div>
       </section>
     </>
--- src/components/Hints.js
+++ src/components/Hints.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 function Hints({ activeAgent }) {
   const [isClass, setClass] = React.useState(true);
   const toggleClass = () => {
@@ -9,11 +8,17 @@ function Hints({ activeAgent }) {
 
   return (
     <>
-      <button className='button is-white is-outlined' onClick={toggleClass}>Show hint!</button>
-      <p id='hints' className={isClass ? 'showing-hint' : null}>{activeAgent && activeAgent.description}</p>
+      <button
+        className="button is-white is-outlined hint-btn"
+        onClick={toggleClass}
+      >
+        Show hint!
+      </button>
+      <p id="hints" className={isClass ? 'showing-hint' : null}>
+        {activeAgent && activeAgent.description}
+      </p>
     </>
   );
-
 }
 
 export default Hints;
